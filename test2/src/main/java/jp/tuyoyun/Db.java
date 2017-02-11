abe5dd99ecd28454aff7700007dda3a8ae96fa09
lightedblue <lightedblue@gmail.com>
2017-02-12 01:00:57 +0900

    addtran

--- test2/src/main/java/jp/tuyoyun/Db.java
+++ test2/src/main/java/jp/tuyoyun/Db.java
@@ -2,19 +2,44 @@ package jp.tuyoyun;
 
 import org.apache.ibatis.session.ResultContext;
 import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.mule.api.MuleEventContext;
 import org.mule.api.lifecycle.Callable;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import jp.cloudear.test.db.Test3;
+import jp.cloudear.test.db.Test3Mapper;
 
 public class Db implements Callable {
+
+	@Autowired
+	Test3Mapper mapperBase;
+
 	@Autowired
 	Test3MapperEx mapper;
 
+	@Autowired
+	SqlSessionFactoryBean sessionFactoryBean;
+
 	@Override
 	public Object onCall(MuleEventContext eventContext) throws Exception {
-		resultHandler();
+		// resultHandler();
+
+		SqlSessionFactory sessionFactory = sessionFactoryBean.getObject();
+		SqlSession session = sessionFactory.openSession();
+
+		Test3 record = new Test3();
+		record.setTitle("test6");
+		record.setRssUrl("http://test6");
+		session.insert("insert", record);
+		session.commit();
+		// session.rollback();
+
+		insert();
+		System.out.println();
 
 		// try (Cursor<Test3> todos = mapper.selectCursor()) { //
 		// try-with-resources文でCursorを確実にクローズする
@@ -26,6 +51,14 @@ public class Db implements Callable {
 		// }
 
 		return "ok";
+	}
+
+	@Transactional
+	private void insert() {
+		Test3 record = new Test3();
+		record.setTitle("test7");
+		record.setRssUrl("http://test7");
+		mapperBase.insert(record);
 	}
 
 	private void resultHandler() {
